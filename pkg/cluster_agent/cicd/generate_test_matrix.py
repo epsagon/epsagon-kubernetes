258a3615b19dc2da92dc724368717bc560a03e28
Maor Levi <maor@epsagon.com>
2021-03-18 20:12:29 +0200

    Fixing step outputs

--- pkg/cluster_agent/cicd/generate_test_matrix.py
+++ pkg/cluster_agent/cicd/generate_test_matrix.py
@@ -7,7 +7,7 @@ import json
 
 SYSTEM_TEST_DIRPATH= "tests/system/"
 
-def main(args):
+def main():
     dirs = [result[0] for result in os.walk(SYSTEM_TEST_DIRPATH)]
     dirs.remove(SYSTEM_TEST_DIRPATH)
     print(json.dumps(dirs))
