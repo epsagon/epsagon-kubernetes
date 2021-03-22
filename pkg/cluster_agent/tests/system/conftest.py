17d727405cca35b7b7ccf1ed113bf4ccdbd4e9c5
Maor Levi <maor@epsagon.com>
2021-03-22 10:44:30 +0200

    Fixing CR

--- pkg/cluster_agent/tests/system/conftest.py
+++ pkg/cluster_agent/tests/system/conftest.py
@@ -88,7 +88,7 @@ class ClusterAgentInstaller:
         """
         Waits for one a pod of the given deployment to be ready
         """
-        timeout = timedelta(seconds=10)
+        timeout = timedelta(seconds=30)
         start = datetime.now()
         end = datetime.now()
         while end - start < timeout:
